130f9824c894d242d086eb454a02304a794a2e72
winstencoellins <winstencoellins13@gmail.com>
2025-01-11 00:48:10 +0800

    add: functionality of products page

--- utils/products.ts
+++ utils/products.ts
@@ -1,5 +1,16 @@
+/**
+ * Products will be divided into four categories:
+ * 1. Printer -> printer
+ * 2. Thermal Paper -> thermalPaper
+ * 3. Ink -> ink
+ * 4. Ribbon Cartridge -> ribbonCartridge
+ */
+
 const products = [
-    {id: 1, src: "", title: "", desc: "", shopeeLink: "", topedLink: ""}
+    {id: 1, src: "", alt:"", title: "Printer 1", desc: "Lorem ipsum dolor sit amet aliqua.", category: "printer", shopeeLink: "", topedLink: ""},
+    {id: 2, src: "", alt:"", title: "Thermal Paper 1", desc: "Lorem ipsum dolor sit amet aliqua.", category: "thermalPaper", shopeeLink: "", topedLink: ""},
+    {id: 3, src: "", alt:"", title: "Ink 1", desc: "Lorem ipsum dolor sit amet aliqua.", category: "ink", shopeeLink: "", topedLink: ""},
+    {id: 4, src: "", alt:"", title: "Ribbon Cartridge 1", desc: "Lorem ipsum dolor sit amet aliqua.", category: "ribbonCartridge", shopeeLink: "", topedLink: ""},
 ]
 
-export default { products }
+export default products
